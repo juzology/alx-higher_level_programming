ba7251002f73a50436c5f59c6bce53ae1b0af75c
juzology <juzology007@gmail.com>
2023-03-06 11:19:17 -0800

    hello world, python

--- 0x00-python-hello_world/list.h
+++ 0x00-python-hello_world/list.h
@@ -1,27 +1,25 @@
-#ifndef LIST_H
-#define LIST_H
+#ifndef LISTS_H
+#define LISTS_H
 
-
-#include <stdio.h>
+#include <stdlib.h>
 
 /**
  * struct listint_s - singly linked list
- * @n: interger
+ * @n: integer
  * @next: points to the next node
  *
- * description: singly linked list node structure
- * for alx project
+ * Description: singly linked list node structure
+ * for Holberton project
  */
-
-typdef struct listint_s
+typedef struct listint_s
 {
 	int n;
 	struct listint_s *next;
 } listint_t;
 
-sized_t print_listint(const listint_n *h);
+size_t print_listint(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
 void free_listint(listint_t *head);
 int check_cycle(listint_t *list);
 
-#endif
+#endif /* LISTS_H */
