cdcb9146e716f0a9ff163c4a14e06567dd1202dc
juzology <juzoma007@gmail.com>
2023-03-09 18:33:03 +0000

    import_modules

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,10 +1,7 @@
-
-m add_0 import add
-
+#!/usr/bin/python3
 if __name__ == "__main__":
-    """
-    Prints the result of the addition between two numbers
-    """
+    from add_0 import add
+
     a = 1
     b = 2
-    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+    print('{:d} + {:d} = {:d}'.format(a, b, add(a, b)))
